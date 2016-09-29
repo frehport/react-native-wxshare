f4f2337e75c045df4f440e09e2712c177064b234
李蒙 <limeng@novacloud.com>
2016-09-29 11:21:24 +0800

    wx share web url

--- android/src/main/java/com/novacloud/wxshare/react/WxShareNativeModule.java
+++ android/src/main/java/com/novacloud/wxshare/react/WxShareNativeModule.java
@@ -1,17 +1,26 @@
 package com.novacloud.wxshare.react;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.util.Log;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.ReadableMap;
 import com.tencent.mm.sdk.modelmsg.SendMessageToWX;
 import com.tencent.mm.sdk.modelmsg.WXMediaMessage;
 import com.tencent.mm.sdk.modelmsg.WXTextObject;
+import com.tencent.mm.sdk.modelmsg.WXWebpageObject;
 import com.tencent.mm.sdk.openapi.IWXAPI;
 import com.tencent.mm.sdk.openapi.WXAPIFactory;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.UUID;
 
 
@@ -22,6 +31,11 @@ import java.util.UUID;
 public class WxShareNativeModule extends ReactContextBaseJavaModule {
 
     private IWXAPI api;
+    private static final String TITLE = "title";
+    private static final String URL = "url";
+    private static final String IMAGE_URL = "imageURL";
+    private static final String DESCRIPTION = "description";
+
 
     public WxShareNativeModule(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -39,7 +53,7 @@ public class WxShareNativeModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void share(ReadableArray array) {
+    public void shareText(ReadableArray array) {
         WXTextObject textObject = new WXTextObject();
         String text =  WxShareHelper.formatData(array);
 
@@ -55,7 +69,57 @@ public class WxShareNativeModule extends ReactContextBaseJavaModule {
         req.scene = SendMessageToWX.Req.WXSceneSession;
 
         api.sendReq(req);
-
     }
+
+    @ReactMethod
+    public void share(ReadableMap map) {
+        SendMessageToWX.Req req = new SendMessageToWX.Req();
+        req.transaction = UUID.randomUUID().toString();
+
+        if (map.hasKey(URL)) {
+            WXWebpageObject webpageObject = new WXWebpageObject();
+            webpageObject.webpageUrl = map.getString(URL);
+            WXMediaMessage msg = new WXMediaMessage(webpageObject);
+            msg.title = map.getString(TITLE);
+            if (map.hasKey(DESCRIPTION)) msg.description = map.getString(DESCRIPTION);
+            if (map.hasKey(IMAGE_URL)) msg.setThumbImage(getBitMap(map.getString(IMAGE_URL)));
+            req.message = msg;
+
+        } else {
+            String text = map.getString(TITLE);
+            WXTextObject textObject = new WXTextObject();
+            textObject.text = text;
+
+            WXMediaMessage msg = new WXMediaMessage();
+            msg.mediaObject = textObject;
+            msg.description = text;
+
+            req.message = msg;
+        }
+        req.scene = SendMessageToWX.Req.WXSceneSession;
+        api.sendReq(req);
+    }
+
+    private Bitmap getBitMap(String url) {
+        URL fileURl = null;
+        Bitmap bitmap = null;
+        try {
+            fileURl = new URL(url);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            HttpURLConnection conn = (HttpURLConnection) fileURl.openConnection();
+            conn.setDoInput(true);
+            conn.connect();
+            InputStream is = conn.getInputStream();
+            bitmap = BitmapFactory.decodeStream(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bitmap;
+    }
+
 
 }
--- ios/WxShare/WxShare.m
+++ ios/WxShare/WxShare.m
@@ -25,12 +25,37 @@ RCT_EXPORT_METHOD(registerApp:(NSString* )appKey) {
     [WXApi registerApp:appKey];
 }
 
-RCT_EXPORT_METHOD(share:(NSArray *)array) {
-        SendMessageToWXReq *req = [[SendMessageToWXReq alloc] init];
-        req.text = [WxShareDataFormatter formatData:array];
-        req.bText = YES;
-        req.scene = WXSceneSession;
-        [WXApi sendReq:req];
+RCT_EXPORT_METHOD(shareText:(NSArray *)array) {
+    SendMessageToWXReq *req = [[SendMessageToWXReq alloc] init];
+    req.text = [WxShareDataFormatter formatData:array];
+    req.bText = YES;
+    req.scene = WXSceneSession;
+    [WXApi sendReq:req];
+}
+
+RCT_EXPORT_METHOD(share:(NSDictionary *)dic) {
+    SendMessageToWXReq *req = [[SendMessageToWXReq alloc] init];
+    req.scene = WXSceneSession;
+    if (dic[@"url"]) {
+        req.bText = NO;
+        WXMediaMessage *message = [WXMediaMessage message];
+        message.title = dic[@"title"];
+        message.description = dic[@"description"];
+        if (dic[@"imageURL"]) {
+            [message setThumbImage:[UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:dic[@"imageURL"]]]]];
+        }
+        
+        WXWebpageObject *webpageObject = [WXWebpageObject object];
+        webpageObject.webpageUrl = dic[@"url"];
+        message.mediaObject = webpageObject;
+        
+        req.message = message;
+        
+    } else {
+        req.text = dic[@"text"];
+    }
+    
+    [WXApi sendReq:req];
 }
 
 @end
